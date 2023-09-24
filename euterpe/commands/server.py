64ea2f73b154c647f90e66758cd594ed96a7d670
Brice Santus <brice.santus@proton.me>
2023-09-24 13:20:58 +0200

    feat: adding server run first command

--- euterpe/commands/server.py
+++ euterpe/commands/server.py
@@ -21,7 +21,7 @@ def get_account():
 
 @app.command()
 def play():
-    print("[blue]Starting server...[/blue]")
+    print("[blue]Play demo ...[/blue]")
     account = get_account()
     scope = "user-read-playback-state,user-modify-playback-state"
     sp = spotipy.Spotify(
@@ -39,6 +39,37 @@ def play():
     print(f"[blue]Currently playing:[/blue] {track['name']} by {track['artists'][0]['name']}")
     # Change track
     sp.start_playback(uris=[test_uri], device_id=device["id"])
+
+
+@app.command()
+def run():
+    print("[blue]Starting server...[/blue]")
+    reader = SimpleMFRC522()
+    scope = "user-read-playback-state,user-modify-playback-state"
+    sp = spotipy.Spotify(
+        auth_manager=SpotifyOAuth(
+            client_id=account["client_id"],
+            client_secret=account["client_secret"],
+            redirect_uri="http://localhost:8000/callback",
+            scope=scope,
+            open_browser=False,
+        )
+    )
+    device = [d for d in sp.devices()["devices"] if d["name"] == test_device][0]
+
+    while True:
+        try:
+            id, text = reader.read()
+            print(f"Reading card {id}...")
+            print(f"Search corresponding peering ...")
+            r = requests.get(f"http://localhost:8000/peerings/{id}")
+            peering = r.json()
+            print(f"Found peering: {peering}")
+            print(f"Play track {peering['spotify_id']} ...")
+            track = sp.track(test_uri)
+            sp.start_playback(uris=[test_uri], device_id=device["id"])
+        finally:
+            GPIO.cleanup()
 
 
 @app.command()
